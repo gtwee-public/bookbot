a839f2548ed169ce44c03c7315f93bf83e99bb3c
Gunnar Tweeton <gtweeton@gmail.com>
2025-10-24 23:28:21 -0500

    CH2 L6: Count Characters

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
     from stats import get_num_words
+    from stats import count_each_character
     get_num_words()
+    count_each_character()
+
 
 main()
 
--- stats.py
+++ stats.py
@@ -10,3 +10,16 @@ def split_words(str):
 
 def get_num_words():
     print(f" Found {split_words(get_book_text())} total words")
+
+def count_each_character():
+    book_text = get_book_text().lower()
+    text_count = {}
+
+    for text in book_text:
+        if text in text_count:
+            text_count[text] += 1
+        else:
+            text_count[text] = 1
+
+    print(text_count)
+

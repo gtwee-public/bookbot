3e43a0458d448cdeaea9eb6019a811f0d3f58e54
Gunnar Tweeton <gtweeton@gmail.com>
2025-10-28 10:35:03 -0500

    CH2 L2: Arguments

--- main.py
+++ main.py
@@ -3,11 +3,21 @@
 # This file will take in functions and stats from other files 
 # to compile the data into a nicely organized manner.
 
+import sys
+file_location = ""
+
 # The main function body of program
 def main():
+    # Checks if sys.argv has two entries, stops and explains how
+    # to use program if it doesn't.
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        file_location = sys.argv[1]
+
     from stats import get_num_words
     from stats import split_and_sort
-    from stats import file_location
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_location}...")
--- stats.py
+++ stats.py
@@ -1,21 +1,26 @@
-file_location = "books/frankenstein.txt"
+import sys
 
+# Function will get the contents of the file used for analsys
+# and return it as a string
 def get_book_text():
-    with open(file_location) as f:
+    with open(sys.argv[1]) as f:
         file_contents = f.read()
         return file_contents
 
+# Function will take an str as input and split the string into
+# words, then it will return the amount of words from that string
 def split_words(str):
-    words = str.split()
-    count = len(words)
+    count = len(str.split())
     return count
 
+# Function will use the split_words() and get_book_text() functions
+# to print the total number of words in the file into a neat sentence
 def get_num_words():
     print(f" Found {split_words(get_book_text())} total words")
 
-def sort_characters(characters):
-    return characters["data"]
-
+# Function will take the string of text from get_book_text() function
+# and return a count of each individual character as a key pair
+# within a dictionary e.g {individual_character: number_of_instances}
 def count_each_character():
     book_text = get_book_text().lower()
     text_count = {}
@@ -27,6 +32,10 @@ def count_each_character():
             text_count[text] = 1
     return text_count
 
+# Function will take the returned dictionary from count_each_character()
+# function and split it into seperate dictionaries per key.
+# Then it will sort the dictionaries by value within a list and print
+# that sorted list neatly.
 def split_and_sort():
     dictionary = count_each_character()
 

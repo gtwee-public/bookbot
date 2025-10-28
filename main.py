26322b38476341e2635dd1226895290081fb6c74
Gunnar Tweeton <gtweeton@gmail.com>
2025-10-28 09:39:52 -0500

    CH3 L1: Print a Report

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+# Bookbot main.py
+
+# This file will take in functions and stats from other files 
+# to compile the data into a nicely organized manner.
+
+# The main function body of program
 def main():
     from stats import get_num_words
-    from stats import count_each_character
+    from stats import split_and_sort
+    from stats import file_location
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_location}...")
+    print("----------- Word Count ----------")
+    
+    # Function will take in predetermined file location and return
+    # the total number of words in the file.
     get_num_words()
-    count_each_character()
+    
+    print("--------- Character Count -------")
+
+    # Function will count and sort each individual character from 
+    # the file and print the info into the terminal.
+    split_and_sort()
+
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,7 @@
+file_location = "books/frankenstein.txt"
+
 def get_book_text():
-    with open("books/frankenstein.txt") as f:
+    with open(file_location) as f:
         file_contents = f.read()
         return file_contents
 
@@ -11,15 +13,31 @@ def split_words(str):
 def get_num_words():
     print(f" Found {split_words(get_book_text())} total words")
 
+def sort_characters(characters):
+    return characters["data"]
+
 def count_each_character():
     book_text = get_book_text().lower()
     text_count = {}
-
+    
     for text in book_text:
         if text in text_count:
             text_count[text] += 1
         else:
             text_count[text] = 1
+    return text_count
 
-    print(text_count)
+def split_and_sort():
+    dictionary = count_each_character()
+
+    # Convert each key-value pair into a separate dictionary
+    individual_dicts = {k: v for k, v in dictionary.items() if k != ' '}
+
+    # Sort the dictionary by value (descending)
+    sorted_list = sorted(individual_dicts.items(), key=lambda x: x[1], reverse=True)
+
+    # Print them line by line
+    for item in sorted_list:
+        print(f"{item[0]}: {item[1]}")
+
 
